2dd34962d4d8acd3cce4a21c6b2dfcf8a046c18f
vivek <vivekkush05@gmail.com>
2018-04-11 16:49:05 +0530

    fist commit

---
+++ src/pages/home/home.ts
@@ -0,0 +1,28 @@
+
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+import { SettingsProvider } from '../../providers/settings';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+	 selectedTheme: String;
+
+  constructor(public navCtrl: NavController,private settings: SettingsProvider) {
+	   this.settings.getActiveTheme().subscribe(val => this.selectedTheme = val);
+
+  }
+
+	 toggleAppTheme() {
+    if (this.selectedTheme === 'dark-theme') {
+      this.settings.setActiveTheme('light-theme');
+    } else {
+      this.settings.setActiveTheme('dark-theme');
+    }
+  }
+
+
+  
+}
